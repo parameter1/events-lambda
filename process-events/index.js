1c0e83c27bcd2eb3d92a6ac0c0a28d3a4c8882f3
Jacob Bare <jacob@parameter1.com>
2021-03-05 08:43:22 -0600

    Send event messages to archive queue

--- process-events/index.js
+++ process-events/index.js
@@ -1,21 +1,41 @@
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const { nanoid } = require('nanoid');
 const mongodb = require('./mongodb');
 
 const db = mongodb();
+const sqs = new AWS.SQS();
+
+const { ARCHIVE_QUEUE_URL } = process.env;
 
 exports.handler = async (event, context) => {
   // see https://docs.atlas.mongodb.com/best-practices-connecting-to-aws-lambda/
   context.callbackWaitsForEmptyEventLoop = false;
+
+  const toArchiveEntries = [];
 
   /**
    * Map all records to the appropriate tenant slug.
    */
   const opsMap = event.Records.reduce((map, record) => {
     const doc = JSON.parse(record.body);
+
+    // push data to the archive batch.
+    toArchiveEntries.push({
+      Id: nanoid(10),
+      MessageBody: record.body,
+      MessageDeduplicationId: doc._id,
+      MessageGroupId: doc.slug,
+    });
+
     const { slug } = doc;
     if (!map.has(slug)) map.set(slug, []);
 
+    // replace the timestamp with a Date
     const date = new Date(doc.ts);
     delete doc.ts;
+
+    // delete the slug. no need to save it once we've determined the DB
+    delete doc.slug;
 
     const filter = { _id: doc._id };
     const $setOnInsert = { ...doc, date };
@@ -27,9 +47,12 @@ exports.handler = async (event, context) => {
   /**
    * Convert map into an array of bulk write operations per tenant.
    */
-  await Promise.all(Array.from(opsMap, ([slug, ops]) => ({ slug, ops })).map(async ({ slug, ops }) => {
-    const dbName = `p1-events-${slug}`;
-    const collection = await db.collection({ dbName, name: 'events' });
-    await collection.bulkWrite(ops);
-  }));
+  await Promise.all(Array.from(opsMap, ([slug, ops]) => ({ slug, ops }))
+    .map(async ({ slug, ops }) => {
+      const dbName = `p1-events-${slug}`;
+      const collection = await db.collection({ dbName, name: 'events' });
+      await collection.bulkWrite(ops);
+    }));
+
+  await sqs.sendMessageBatch({ QueueUrl: ARCHIVE_QUEUE_URL, Entries: toArchiveEntries }).promise();
 };
